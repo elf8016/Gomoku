0d49e814824daba65a93e07d73bd765f3c05c9ab
Zhouyiyang69 <agou83156411@gmail.com>
2023-05-25 00:28:36 +0800

    zyy00

--- 1A2B.py
+++ 1A2B.py
@@ -1,14 +1,26 @@
+from random import randint
 def game(ans) :
     C = 0
+    print("Welcome to 1A2B, Type 'Give up' to quit")
     while C == 0  :
         A = 0
         B = 0 
         guess = input("\033[37mInput 4 num : ") 
+        setguess = set(guess)            
         if str.isdigit(guess) == False : 
             print('\033[31merror!! 4 !num! plz',)
             continue
         if len(guess) != 4 :
             print('\033[31merror!! !4! num plz',)
+            continue
+        if guess[0] == '0' :
+            print('\033[31merror!! first num cannot be 0',)
+            continue
+        if ans == 'Give up' :
+            C = C + 1
+            continue
+        if len(setguess) != len(guess) :
+            print("\033[37mInput 4 !different! num : ")
             continue
         for i  in range(0,4) :
             for j in range(0,4):
@@ -21,17 +33,12 @@ def game(ans) :
             print("Won!!")
             C = C + 1
         else : 
-            print( A,end='')
-            print('A',end='')
-            print(B,end='')
-            print('B, ',end='')
-            print('try again, ',end='')
+            print("\033[37m",A,'A',B,'B, Try again!!',sep="")
 def generateans() : 
-    from random import randint
     c = ''
     while len(c) < 4 : 
         b = randint(0,9)
-        if len(c) == 0 :
+        if len(c) == 0 and  b == 0 :
             if b == 0 :
                 continue
         c = c + str(b)
