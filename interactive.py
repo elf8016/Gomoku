6ae3ae0e92e5fa5b7a5efd77ae475661c3c1ee9a
AbudyWu <B11007143@gapps.ntust.edu.tw>
2023-06-02 16:22:31 +0800

    dev 1.2

--- interactive.py
+++ interactive.py
@@ -3,7 +3,7 @@ import variable
 
 
 def init() -> List[int]:
-    variable.chessboard = [[1 for i in range(0, 11)] for j in range(0, 11)]
+    variable.chessboard = [[0 for i in range(0, 11)] for j in range(0, 11)]
     variable.track = []
     # global track
     # global chessboard
